b80c37aecb5532902efcab618b23fd6b763be203
Ross Comer MainPC <122069684+yankee3313@users.noreply.github.com>
2023-12-01 00:15:54 -0600

    test with no skip

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/version', (req, res) => {
-  res.send('8') // change this string to ensure a new version deployed
+  res.send('9') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
